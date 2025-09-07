fb3fe5417f64729a59264abc5473e5cc2a4bb689
dzulfikar05 <fikaralghifari0504@gmail.com>
2025-09-07 17:00:31 +0700

    variable

--- bin/project1.dart
+++ bin/project1.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:project1/project1.dart' as project1;
 
 
@@ -13,7 +15,21 @@ void main(List<String> arguments) {
 
   // constanta dan final
   final String nama2 = 'Muh'; 
-
   print("Nama saya $nama $nama2");
 
+  // variable
+  int umur = 20;
+  double tinggi = 140.2;
+  bool jenisKelamin = true; // true = laki-laki, false = perempuan
+  String alamat = "Jl. Raya";
+
+  stdout.writeln("Umur saya $umur");
+  stdout.writeln("Tinggi saya $tinggi");
+  stdout.writeln("Jenis kelamin saya ${jenisKelamin ? 'laki-laki' : 'perempuan'}");
+  // if (jenisKelamin) {
+  //   stdout.writeln("Jenis kelamin saya laki-laki");
+  // } else {
+  //   stdout.writeln("Jenis kelamin saya perempuan");
+  // }
+  stdout.writeln("Alamat saya $alamat");
 }
