8e466b6f8fa2ca271ac30ba7c56107a02ca3c652
dzulfikar05 <fikaralghifari0504@gmail.com>
2025-09-19 13:07:00 +0700

    list

---
+++ pertemuan4/bin/pertemuan4.dart
@@ -0,0 +1,19 @@
+import 'dart:io';
+
+import 'package:pertemuan4/pertemuan4.dart' as pertemuan4;
+
+void main(List<String> arguments) {
+  // print('Hello world: ${pertemuan4.calculate()}!');
+
+  var fl = List<int>.filled(4, 2);
+  
+  fl[0] = 1;
+  fl[1] = 2;
+  fl[2] = 3;
+  fl[3] = 4;
+  // fl[4] = 5;
+
+  // cetak
+  stdout.writeln(fl);
+
+}
