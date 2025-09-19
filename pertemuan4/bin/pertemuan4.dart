f7e7fdfdd8dd0cbeeb5f1d9bc84e3fef473e9e5e
dzulfikar05 <fikaralghifari0504@gmail.com>
2025-09-19 13:35:25 +0700

    collection

--- pertemuan4/bin/pertemuan4.dart
+++ pertemuan4/bin/pertemuan4.dart
@@ -3,17 +3,49 @@ import 'dart:io';
 import 'package:pertemuan4/pertemuan4.dart' as pertemuan4;
 
 void main(List<String> arguments) {
-  // print('Hello world: ${pertemuan4.calculate()}!');
+  // // print('Hello world: ${pertemuan4.calculate()}!');
 
-  var fl = List<int>.filled(4, 2);
-  
-  fl[0] = 1;
-  fl[1] = 2;
-  fl[2] = 3;
-  fl[3] = 4;
-  // fl[4] = 5;
+  // var fl = List<int>.filled(4, 2);
 
-  // cetak
-  stdout.writeln(fl);
+  // fl[0] = 1;
+  // fl[1] = 2;
+  // fl[2] = 3;
+  // fl[3] = 4;
+  // // fl[4] = 5;
+
+  // // cetak
+  // stdout.writeln(fl);
+
+  // var gl = [1, 2, 3];
+  // gl.add(4);
+  // gl.remove(1);
+
+  // stdout.writeln(gl);
+
+  // var setNilai1 = <int>{1, 2, 3, 4};
+  // var setNilai2 = <int>{3, 3, 4, 5};
+
+  // print(setNilai2);
+  // print(setNilai1);
+
+  // print(setNilai1.union(setNilai2));
+  // print(setNilai1.intersection(setNilai2));
+
+  var setNilai1 = <int>{};
+  var setNilai2 = <int>{1, 2, 3};
+
+  stdout.writeln("Jumlah Data setNilai1 =");
+  String input = stdin.readLineSync()!;
+  int jumlah = int.parse(input);
+  for (var i = 0; i < jumlah; i++) {
+    stdout.writeln("Nilai ke-${i + 1}");
+    String input = stdin.readLineSync()!;
+    int nilai = int.parse(input);
+    setNilai1.add(nilai);
+  }
+
+  print(setNilai1);
+   print(setNilai1.union(setNilai2));
+  print(setNilai1.intersection(setNilai2));
 
 }
