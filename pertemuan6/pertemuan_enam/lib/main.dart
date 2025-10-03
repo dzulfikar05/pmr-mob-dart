d8930caf1db4e1c74adff43f17b71292e2d54053
dzulfikar05 <fikaralghifari0504@gmail.com>
2025-10-03 10:18:28 +0700

    contoh scaffold

---
+++ pertemuan6/pertemuan_enam/lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Hello World'),
+        ),
+        body: const Center(
+          child: Text('Hello World'),
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: const Icon(Icons.add),
+          onPressed: () {},
+        ),
+      )
+    );
+  }
+}
