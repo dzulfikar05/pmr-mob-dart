1f44624cdef24bdbbe5be47ccf0eea7f865f1948
dzulfikar05 <fikaralghifari0504@gmail.com>
2025-10-11 14:41:07 +0700

    contoh SingleChild

--- pertemuan7/pertemuan_tujuh/lib/main.dart
+++ pertemuan7/pertemuan_tujuh/lib/main.dart
@@ -1,58 +1,24 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const GantiWarnaApp());
+  runApp(SingleChildExample());
 }
 
-class GantiWarnaApp extends StatefulWidget {
-  const GantiWarnaApp({super.key});
-
-  @override
-  State<GantiWarnaApp> createState() => _GantiWarnaAppState();
-}
-
-class _GantiWarnaAppState extends State<GantiWarnaApp> {
-  // state untuk menentukan warna & teks
-  bool _isBlue = true; 
-
-  void _ubahWarna() {
-    setState(() {
-      // ubah nilai state
-      _isBlue = !_isBlue; 
-    });
-  }
+class SingleChildExample extends StatelessWidget {
+  const SingleChildExample({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: _isBlue ? Colors.blue[100] : Colors.pink[100],
         appBar: AppBar(
-          title: const Text('Contoh Stateful: Ubah Warna'),
-          backgroundColor: _isBlue ? Colors.blue : Colors.pink,
+          title: Text('Single Child Layout Example'),
+          backgroundColor: Colors.blue,
         ),
         body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              
-              Text(
-                _isBlue ? 'Warna Biru ✈️' : 'Warna Merah Muda 🌸',
-                style: TextStyle(
-                  fontSize: 24,
-                  color: _isBlue ? Colors.blue[900] : Colors.pink[900],
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              const SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: _ubahWarna,
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: _isBlue ? Colors.blue : Colors.pink,
-                ),
-                child: const Text('Ubah Warna'),
-              ),
-            ],
+          child: Text(
+            'Hello Flutter!',
+            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
           ),
         ),
       ),
