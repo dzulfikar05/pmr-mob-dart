b92039b5df4d9e4ef6e839c2c0f06f58a141cece
dzulfikar05 <fikaralghifari0504@gmail.com>
2025-10-11 14:39:17 +0700

    contoh stateful

--- pertemuan7/pertemuan_tujuh/lib/main.dart
+++ pertemuan7/pertemuan_tujuh/lib/main.dart
@@ -1,24 +1,58 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyStatelessApp());
+  runApp(const GantiWarnaApp());
 }
 
-class MyStatelessApp extends StatelessWidget {
-  const MyStatelessApp({super.key});
+class GantiWarnaApp extends StatefulWidget {
+  const GantiWarnaApp({super.key});
+
+  @override
+  State<GantiWarnaApp> createState() => _GantiWarnaAppState();
+}
+
+class _GantiWarnaAppState extends State<GantiWarnaApp> {
+  // state untuk menentukan warna & teks
+  bool _isBlue = true; 
+
+  void _ubahWarna() {
+    setState(() {
+      // ubah nilai state
+      _isBlue = !_isBlue; 
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: _isBlue ? Colors.blue[100] : Colors.pink[100],
         appBar: AppBar(
-          title: const Text('Contoh Stateless Widget'),
-          backgroundColor: Colors.blue,
+          title: const Text('Contoh Stateful: Ubah Warna'),
+          backgroundColor: _isBlue ? Colors.blue : Colors.pink,
         ),
-        body: const Center(
-          child: Text(
-            'Halo, saya Stateless Widget!',
-            style: TextStyle(fontSize: 20),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              
+              Text(
+                _isBlue ? 'Warna Biru ✈️' : 'Warna Merah Muda 🌸',
+                style: TextStyle(
+                  fontSize: 24,
+                  color: _isBlue ? Colors.blue[900] : Colors.pink[900],
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: _ubahWarna,
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: _isBlue ? Colors.blue : Colors.pink,
+                ),
+                child: const Text('Ubah Warna'),
+              ),
+            ],
           ),
         ),
       ),
