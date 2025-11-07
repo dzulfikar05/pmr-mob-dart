f73b5f24ce496b41be2180b1010a9d0fa6dc39aa
dzulfikar05 <fikaralghifari0504@gmail.com>
2025-11-07 14:19:11 +0700

    konsep state local

--- pertemuan11/pertemuan_sebelas/lib/main.dart
+++ pertemuan11/pertemuan_sebelas/lib/main.dart
@@ -1,65 +1,36 @@
 import 'package:flutter/material.dart';
-import 'models/counter_model.dart';
 
-void main() => runApp(const MyApp());
+void main() => runApp(const MaterialApp(home: CounterDemo()));
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: CounterPage(),
-    );
-  }
-}
-
-class CounterPage extends StatefulWidget {
-  const CounterPage({super.key});
+class CounterDemo extends StatefulWidget {
+  const CounterDemo({super.key});
 
   @override
-  State<CounterPage> createState() => _CounterPageState();
+  State<CounterDemo> createState() => _CounterDemoState();
 }
 
-class _CounterPageState extends State<CounterPage> {
-  final CounterModel counter = CounterModel(); // <-- Controller pakai model
+class _CounterDemoState extends State<CounterDemo> {
+  int counter = 0;
 
-  void _increment() {
+  void increment() {
     setState(() {
-      counter.increment();
-    });
-  }
-
-  void _reset() {
-    setState(() {
-      counter.reset();
+      counter++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("MVC Counter Example")),
+      appBar: AppBar(title: const Text('Stateful Example')),
       body: Center(
         child: Text(
-          'Nilai Counter: ${counter.value}',
-          style: const TextStyle(fontSize: 28),
+          'Counter: $counter',
+          style: const TextStyle(fontSize: 32),
         ),
       ),
-      floatingActionButton: Column(
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: [
-          FloatingActionButton(
-            onPressed: _increment,
-            child: const Icon(Icons.add),
-          ),
-          const SizedBox(height: 10),
-          FloatingActionButton(
-            onPressed: _reset,
-            backgroundColor: Colors.red,
-            child: const Icon(Icons.refresh),
-          ),
-        ],
+      floatingActionButton: FloatingActionButton(
+        onPressed: increment,
+        child: const Icon(Icons.add),
       ),
     );
   }
