068880ae2f8cb236e94bfcd59c0af84e7cbb3659
dzulfikar05 <fikaralghifari0504@gmail.com>
2025-11-21 10:35:09 +0700

    rename

--- kuis2/json_dua_belas/android/app/src/main/kotlin/com/example/json_dua_belas/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.json_dua_belas
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- kuis2/json_pertemuan_dua_belas/android/app/src/main/kotlin/com/example/json_pertemuan_dua_belas/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.json_pertemuan_dua_belas
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
